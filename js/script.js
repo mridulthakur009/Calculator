b22c20856bda2ccf9dad9f2dee25faaf2ad4a410
mridulthakur009 <mridul.thakur.007@gmail.com>
2023-10-31 18:57:26 +0530

    code modified

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    return a+b;
+}
+
+function clearRec(){
+    document.getElementById("inputBox").value = "";
+}
+function val(num){
+    document.getElementById("inputBox").value += num;
+}
+function equal(){
+
+}
